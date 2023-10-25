03fabc8b39500806a6da5fee10af8c46d9c5c46a
estiak-himel <estiakhimel9@gmail.com>
2023-10-25 21:18:21 +0600

    first commit

---
+++ sticky header.js
@@ -0,0 +1,10 @@
+$(window).scroll(function(){
+    var scrolling = $(this).scrollTop();
+    
+    if(scrolling > 200){
+        $('.navbar').addClass('my_stick_bg');
+    }
+    else {
+         $('.navbar').removeClass('my_stick_bg');
+    }
+    });  
